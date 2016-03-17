3954dacba698ca5564d2ae57bbc69fe31c65470a
maliaoreta <maliaoreta@gmail.com>
2016-03-16 16:50:24 -1000

    takeItem is a func

--- zombies.js
+++ zombies.js
@@ -1,92 +1,24 @@
-/**
- * Class => Item(name)
- * -----------------------------
- * Creates an item.
- *
- * @name Item
- * @param {string} name     The item's name.
- * @property {string} name
- */
+
 function Item (name) {
   this.name = name;
 }
 
-/**
- * Class => Weapon(name, damage)
- * -----------------------------
- * Creates a weapon item.
- * Weapon items can be equipped for use in battle.
- *
- * The Weapon class constructor will call 
- *   the super class (Item) constructor
- *   while passing in the 1 Item constructor param
- *
- * @name Weapon
- * @param {string} name     The weapon's name.
- * @param {number} damage   The weapon's damage.
- * @property {number} damage
- */
 function Weapon (name, damage) {
   
   this.damage = damage;
   Item.call(this, name);
 }
 
-/**
- * Weapon Extends Item Class
- * -----------------------------
- */
 Weapon.prototype = Object.create(Item.prototype);
 
-
-/**
- * Class => Food(name, energy)
- * -----------------------------
- * Creates a food item.
- * Food items give energy, restoring health to the player.
- *
- * The Food class constructor will call 
- *   the super class (Item) constructor
- *   while passing in the 1 Item constructor param
- *
- * @name Food
- * @param {string} name       The food's name.
- * @param {number} energy     The energy the food provides.
- * @property {number} energy
- */
 function Food (name, energy) {
   this.energy = energy;
   Item.call(this, name);
 }
 
-/**
- * Food Extends Item Class
- * -----------------------------
- */
 Food.prototype = Object.create(Item.prototype);
 
 
-/**
- * Class => Player(name, health, strength, speed)
- * -----------------------------
- * Creates a player in a zombie-infested world.
- *
- * @name Player
- * @param {string} name                    The player's name.
- * @param {number} health                  The player's health.
- * @param {number} strength                The player's strength.
- * @param {number} speed                   The player's speed.
- * @private {array} pack                   Default value should be empty.
- * @private {number} maxHealth             Default value should be set to `health`.
- * @property {string} name
- * @property {number} health
- * @property {number} strength
- * @property {number} speed
- * @property {boolean} isAlive             Default value should be `true`.
- * @property {Weapon/boolean} equipped     Default value should be `false`.
- * @property {method} getPack              Returns private variable `pack`.
- * @property {method} getMaxHealth         Returns private variable `maxHealth`.
- */
 function Player (name, health, strength, speed) {
 
   var _pack = [];
@@ -104,6 +36,10 @@ function Player (name, health, strength, speed) {
 
   this.getMaxHealth = function () {
     return _maxHealth;
+  }
+  
+  this.takeItem = function (item) {
+    
   }
 }
 
